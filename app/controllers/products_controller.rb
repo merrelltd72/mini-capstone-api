6e3d1474e8279a856adf805836bfe70d83305a53
merrelltd72 <tyron.merrell@gmail.com>
2024-07-11 16:09:30 -0500

    Added update and delete routes and methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,4 +18,23 @@ class ProductsController < ApplicationController
     )
     render :show
   end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+
+      name: params[:name] || @product.name,
+      description: params[:description] || @product.description,
+      image_url: params[:image_url] || @product.image_url,
+      price: params[:price] || @product.price,
+    )
+
+    render :show
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "Successfully removed the product!"}
+  end
 end
