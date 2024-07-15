f00fc209f32d236cac8673c297179f5ae63ca53c
merrelltd72 <tyron.merrell@gmail.com>
2024-07-15 15:37:07 -0500

    Added validation rules to product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 1..500 }
+
   def is_discounted?
     price.to_i <= 10  
   end
