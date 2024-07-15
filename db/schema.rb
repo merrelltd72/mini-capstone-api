379133749a2c3bc1474bbb561b78e0044db8ad48
merrelltd72 <tyron.merrell@gmail.com>
2024-07-15 13:40:10 -0500

    updated database attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_165934) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_15_182521) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.string "image_url"
-    t.integer "price"
+    t.decimal "price", precision: 9, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "quantity"
   end
 
 end
