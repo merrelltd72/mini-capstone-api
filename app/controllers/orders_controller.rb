b45200939f13fbe95560ee53ec2c1115b926c410
merrelltd72 <tyron.merrell@gmail.com>
2024-07-30 15:59:07 -0500

    Created index, update and destroy methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
 
+  def index
+    @orders = Order.all
+    render :index
+  end
+
   def create
     @order = Order.create(
       user_id: params[:user_id], 
@@ -9,12 +14,39 @@ class OrdersController < ApplicationController
       tax: params[:tax], 
       total: params[:total]
       )
-    render :show, status: 200
+    if @order.valid?
+      render :show, status: 200
+    else
+      render json: { errors: @order.errors.full_messages }, status: 422
+    end
   end
 
   def show
     @order = Order.find_by(id: params[:id])
     render :show
+  end
+
+  def update
+    @order = Order.find_by(id: params[:id])
+    @order.update(
+      quantity: params[:quantity] || @order.quantity, 
+      subtotal: params[:subtotal] || @order.subtotal, 
+      tax: params[:tax] || @order.tax, 
+      total: params[:total] || @order.total
+    )
+    if @order.valid?
+      render :show, status: 200
+    else
+      render json: { 
+        errors: @order.errors.full_messages 
+        }, status: 422
+    end
+  end
+
+  def destroy
+    @order = Order,find_by(id: params[:id])
+    @product.destroy
+    render json: { message: "Order destroyed successfully!" }
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  validates :user_id, presence: true
+  validates :product_id, presence: true
+  validates :quantity, numericality: { greater_than: 1 }
+
   belongs_to :user
   belongs_to :product
 end
