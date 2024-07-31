3d38ea93eef63fbcf8aa679fce73141dbdd0459f
merrelltd72 <tyron.merrell@gmail.com>
2024-07-31 13:39:37 -0500

    Added authorization to orders and product controllers. Updated broken tests.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,36 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
 
   def index
-    @orders = Order.all
+    @orders = current_user.orders
     render :index
   end
 
   def create
+    # Find the product with an id of params[:product_id]
+    # Then grab its price
+    # Then multiply it by params[:quantity]
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    calculated_subtotal = price * params[:quantity].to_i
+    # Multiply the subtotal by the tax rate
+    calculated_tax = calculated_subtotal * 0.09
+    # Add the subtotal and tax
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.create(
-      user_id: params[:user_id], 
-      product_id: params[:product_id], 
-      quantity: params[:quantity], 
-      subtotal: params[:subtotal], 
-      tax: params[:tax], 
-      total: params[:total]
-      )
-    if @order.valid?
-      render :show, status: 200
-    else
-      render json: { errors: @order.errors.full_messages }, status: 422
-    end
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+    render :show
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
+    @order = current_user.orders.find_by(id: params[:id])
     render :show
   end
 
@@ -48,5 +56,7 @@ class OrdersController < ApplicationController
     @product.destroy
     render json: { message: "Order destroyed successfully!" }
   end
+
+
 
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  
+  setup do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", admin: true)
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/products.json"
     assert_response 200
@@ -19,19 +27,28 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "create" do
     assert_difference "Product.count", 1 do
-      post "/products.json", params: { name: "test", price: 10, image_url: "test.jpg", description: "test description", supplier_id: Supplier.first.id }
+      post "/products.json",
+        params: { name: "test", price: 10, image_url: "test.jpg", description: "test description", supplier_id: Supplier.first.id },
+        headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
 
     assert_difference "Product.count", 0 do
-      post "/products.json", params: {}
+      post "/products.json",
+        params: {},
+        headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 422
     end
+
+    post "/products.json", params: {}
+    assert_response 401
   end
 
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Updated name" }
+    patch "/products/#{product.id}.json",
+      params: { name: "Updated name" },
+      headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 200
 
     data = JSON.parse(response.body)
@@ -39,14 +56,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_equal product.price.to_s, data["price"]
     assert_equal product.description, data["description"]
 
-    patch "/products/#{product.id}.json", params: { name: "" }
+    patch "/products/#{product.id}.json",
+      params: { name: "" },
+      headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 422
+
+    patch "/products/#{product.id}.json"
+    assert_response 401
   end
 
   test "destroy" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
+
+    delete "/products/#{Product.first.id}.json"
+    assert_response 401
   end
+  
 end
