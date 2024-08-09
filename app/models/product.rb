fd2c97dbd6ff72f2587f982c1216c53b1c117513
merrelltd72 <tyron.merrell@gmail.com>
2024-08-09 12:37:05 -0500

    Added category model. Created a joined table between category and product and called it CategoryProduct model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,6 +21,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
   #def supplier_id
   #  Supplier.find_by(id: supplier_id)
   #end
